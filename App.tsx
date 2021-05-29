8f78fd07d5dfe6d73264a5dfa290006bda8bbc62
Natan Guedes <nathan.guedes.4@gmail.com>
2021-05-29 16:36:51 -0300

    Fit: Aula 2

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import Routes from './src/routes';
+import { 
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+
+} from '@expo-google-fonts/jost'
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>NLW</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
